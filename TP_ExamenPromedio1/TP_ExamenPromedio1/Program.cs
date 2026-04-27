86ef8ce332bab0a1ecd189f6c6e189a75fc6dc29
Yabely Gutierrez <sebaschg21@gmail.com>
2026-04-27 18:01:25 -0500

    Escritura de las situaciones y opciones

--- TP_ExamenPromedio1/TP_ExamenPromedio1/Program.cs
+++ TP_ExamenPromedio1/TP_ExamenPromedio1/Program.cs
@@ -34,13 +34,15 @@ namespace TP_ExamenPromedio1
         {
             List<SituationCore> list = new List<SituationCore>();
 
-            TextSituation room = new TextSituation("room", "");
-            CombatSituation monster = new CombatSituation("monster", "", 20);
-            TextSituation end = new TextSituation("end", "");
+            TextSituation room = new TextSituation("room", "Te pierdes en el bosque, sin linterna ni artículos que faciliten tu visión. Es de noche y estás solo, así que solo puedes refugiarte en lo que tu creas que es un lugar seguro. " +
+                "Decides dormir, pero cuando estás apunto de lograrlo, escuchas ruidos de pisadas cercanas a tu posición.... algo se acerca");
+            CombatSituation monster = new CombatSituation("monster", "El monstruo salta de entre los escombros realizando un golpe aéreo que logra acertar en tu pierna aunque intentas esquivarlo. Logras golpearlo con una piedra y pararte con dificultad..", 20);
+            TextSituation end = new TextSituation("end", "Logras alejarte, pero el monstruo es más rápido que tú, por lo que eventualmente empieza a alcanzarte. Llegas a una puerta con 3 interruptores que tienen" +
+                " un texto escrito en el centro: Un botón abre la puerta, otro despliega una trampa y otro no acciona nada....tienes que elegir.");
 
-            FinalSituation goodEnding = new FinalSituation("goodEnding", "", "Good");
-            FinalSituation neutralEnding = new FinalSituation("neutralEnding", "", "Neutral");
-            FinalSituation badEnding = new FinalSituation("badEnding", "", "Bad");
+            FinalSituation goodEnding = new FinalSituation("goodEnding", "Abres la puerta y escapas esquivando a duras penas un ataque del monstruo. Por fin estás a salvo", "Good");
+            FinalSituation neutralEnding = new FinalSituation("neutralEnding", "Activas la trampa pero justo antes de caer, el monstruo logra alcanzarte y arrancándote un brazo con sus garras, pero muriendo en el proceso", "Neutral");
+            FinalSituation badEnding = new FinalSituation("badEnding", "Presionas el botón incorrecto y el monstruo logra alcanzarte, atravesándote ferozmente", "Bad");
 
             list.Add(room);
             list.Add(monster);
@@ -49,15 +51,15 @@ namespace TP_ExamenPromedio1
             list.Add(neutralEnding);
             list.Add(badEnding);
 
-            room.options.Add(new OptionCore("", -20, "monster"));
-            room.options.Add(new OptionCore("", -20, "monster"));
+            room.options.Add(new OptionCore("Decides investigar el sonido esperanzado en que solo es la hierba o algún conejo rondandote", -25, "monster"));
+            room.options.Add(new OptionCore("Te mueves silenciosamente lejos de tu ubicación en busca de un mejor refugio, algo más.... seguro", -10, "monster"));
 
-            monster.options.Add(new OptionCore("", -20, "end"));
-            monster.options.Add(new OptionCore("", -20, "end"));
+            monster.options.Add(new OptionCore("Echas a correr lo más rápido que puedes, pero te tropiezas y caes al suelo, clavandote la rodilla con una madera", -15, "end"));
+            monster.options.Add(new OptionCore("Enfrentas al monstruo para intentar dañarlo y ganar más tiempo para escapar", -30, "end"));
 
-            end.options.Add(new OptionCore("", -20, "goodEnding"));
-            end.options.Add(new OptionCore("", -20, "neutralEnding"));
-            end.options.Add(new OptionCore("", -20, "badEnding"));
+            end.options.Add(new OptionCore("Aprietas el botón de la izquierda (Abrir la puerta)", 0, "goodEnding"));
+            end.options.Add(new OptionCore("Aprietas el botón del centro (Despliegas la trampa, pero el monstruo logra atacarte)", -25, "neutralEnding"));
+            end.options.Add(new OptionCore("Aprietas el botón de la derecha (No pasa nada y el monstruo te atraviesa", -100, "badEnding"));
 
 
             return list;
